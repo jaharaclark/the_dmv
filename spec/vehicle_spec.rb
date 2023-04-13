d94145b85caeb4dfc28af38f80bbeea55a4afa71
Jahara Clark <jaharamclark@gmail.com>
2023-04-13 17:01:01 +0900

    facility doesn't register vehicle without service

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -71,5 +71,23 @@ RSpec.describe Vehicle do
       expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
       expect(@facility_1.collected_fees).to eq(325)
     end
+
+    it 'only registers vehicles if the service is provided' do
+      expect(@facility_1.registered_vehicles).to eq([])
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.services).to eq([])
+      @facility_1.register_vehicle(@cruz)
+      @facility_1.register_vehicle(@camaro)
+      @facility_1.register_vehicle(@bolt)
+      @facility_2.register_vehicle(@cruz)
+      @facility_2.register_vehicle(@camaro)
+      @facility_2.register_vehicle(@bolt)
+      expect(@facility_1.registered_vehicles).to eq([@cruz, @camaro, @bolt])
+      expect(@facility_2.registered_vehicles).to eq([])
+      expect(@facility_2.register_vehicle(@cruz)).to eq(nil)
+      expect(@facility_2.register_vehicle(@camaro)).to eq(nil)
+      expect(@facility_2.register_vehicle(@bolt)).to eq(nil)
+      expect(@facility_2.collected_fees).to eq(0)
+    end
   end
 end
