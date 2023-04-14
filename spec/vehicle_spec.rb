5edd2e5636f68cdfc7269a70e9b80eeb141b6135
Jahara Clark <jaharamclark@gmail.com>
2023-04-14 15:00:03 +0900

    Refactor for proper tests on proper spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require './lib/facility'
 require './lib/vehicle'
 
 RSpec.describe Vehicle do
@@ -7,9 +6,6 @@ RSpec.describe Vehicle do
     @cruz = Vehicle.new({vin: '123456789abcdefgh', year: 2012, make: 'Chevrolet', model: 'Cruz', engine: :ice} )
     @bolt = Vehicle.new({vin: '987654321abcdefgh', year: 2019, make: 'Chevrolet', model: 'Bolt', engine: :ev} )
     @camaro = Vehicle.new({vin: '1a2b3c4d5e6f', year: 1969, make: 'Chevrolet', model: 'Camaro', engine: :ice} )
-    @facility_1 = Facility.new({name: 'Albany DMV Office', address: '2242 Santiam Hwy SE Albany OR 97321', phone: '541-967-2014' })
-    @facility_2 = Facility.new({name: 'Ashland DMV Office', address: '600 Tolman Creek Rd Ashland OR 97520', phone: '541-776-6092' })
-    @facility_1.add_service('Vehicle Registration')
   end
   describe '#initialize' do
     it 'can initialize' do
