826c564308962f9a808ed0d714b594c116636d88
Jahara Clark <jaharamclark@gmail.com>
2023-04-15 18:24:29 +0900

    Added renew license test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -112,7 +112,7 @@ RSpec.describe Facility do
       @facility_1.administer_written_test(@registrant_2)
       expect(@registrant_2.license_data).to eq({:written=>true, :license=>false, :renewed=>false})
     end
-    it "it doesn't administer tests or give permits when under 16" do
+    it "it doesn't administer written tests or give permits when under 16" do
       @facility_1.add_service('Written Test')
       expect(@registrant_3.age).to eq(15)
       expect(@registrant_3.permit).to eq(false)
@@ -139,6 +139,25 @@ RSpec.describe Facility do
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
       expect(@facility_1.administer_road_test(@registrant_2)).to eq(true)
       expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
+  end
+
+  describe '#renew_license' do
+    it 'can renew a license' do
+      @facility_1.add_service('Written Test')
+      @facility_1.add_service('Road Test')
+      @facility_1.administer_written_test(@registrant_1)
+      @registrant_2.earn_permit
+      @facility_1.administer_written_test(@registrant_2)
+      expect(@facility_1.renew_drivers_license(@registrant_1)).to eq(nil)
+      @facility_1.add_service('Renew License')
+      expect(@facility_1.services).to eq(['Written Test', 'Road Test', 'Renew License'])
+      @facility_1.renew_drivers_license(@registrant_1)
+      expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>true})
+      @facility_1.renew_drivers_license(@registrant_3)
+      expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+      @facility_1.renew_drivers_license(@registrant_2)
+      expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
 end
